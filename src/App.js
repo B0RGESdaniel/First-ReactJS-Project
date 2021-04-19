fa98d1fb324b6366d11fe20a43a7bea8d9dd181e
Daniel Borges <dlopesborges@gmail.com>
2021-04-19 10:42:08 -0300

    Connecting Front-end with API
    Using GET and POST methods to list all projects and create new ones

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import './App.css';
-import backgroundImage from './assets/background.jfif'
 import Header from './components/Header';
 
 
 function App(){
     
-    const [projects, setProjects] = useState(['Front-end', 'React']);
-    
-    function handleAddProject() {
-        // ... se chama spread operator e serve para percorrer um array e copia todas as suas informações
-        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    const [projects, setProjects] = useState([]);
 
-        console.log(projects)
+    useEffect(() => {
+        api.get('http://localhost:3333/projects').then(response => {
+            setProjects(response.data);
+        })
+    }, []);
+    
+    async function handleAddProject() {
+        
+        const response = await api.post('http://localhost:3333/projects', {
+            title: `Novo Projeto ${Date.now()}`,
+            owner: 'dnbalanoalvo'
+        })
+        // ... se chama spread operator e serve para percorrer um array e copia todas as suas informações
+        // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+        const project = response.data;
+        setProjects([...projects, project])
     }
 
     return (
         <>
             <Header title="Projects" />
 
-            <img width={300} src={backgroundImage}/>
-
             <ul>
-                {projects.map(project => (<li key={project}>{project}</li>))}
+                {projects.map(project => (<li key={project.id}>{project.title}</li>))}
             </ul>
             
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
